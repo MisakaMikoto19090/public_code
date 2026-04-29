386cabf6cced16d156fb1b6809e5fc525f305a4d
MADAONOMORE <weiruancaibi@outlook.com>
2026-04-30 00:27:27 +0800

    ..

--- boxstarter_file/winget_install.260417_必须管理员.ps1
+++ boxstarter_file/winget_install.260417_必须管理员.ps1
@@ -1,4 +1,47 @@
 
+#开机启动
+winget.exe install --id "LiErHeXun.Quicker" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+	# winget.exe install --id "Yuanli.uTools" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809 # alt 空格
+
+#网盘
+winget.exe install --id "Baidu.BaiduNetdisk" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+	## 夸克用沙盒运行
+##网盘
+	winget.exe install --id "gaozhangmin.XBYDriver" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+	## 小白羊运行一次后注释掉
+##工具软件scoop不支持
+winget.exe install --id "Google.EarthPro" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+
+
+##聊天
+winget.exe install --id "Tencent.TIM" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+
+winget.exe install --id "Tonec.InternetDownloadManager" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+#其他
+winget.exe install --id "THS.THS" --location "D:\0LongTermData\0software" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+#沙盒
+winget.exe install --id "Sandboxie.Plus" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+
+
+winget.exe install --id "Tencent.WeixinDevTools" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+
+#workbuddy
+winget.exe install --id "Tencent.WorkBuddy" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+
+
+# IDE
+winget.exe install --id "Microsoft.VisualStudio.Enterprise" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+	## VYGRN-WPR22-HG4X3-692BF-QGT2V
+
 # WSL
 winget.exe install --id "OffSec.KaliLinux" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
 winget.exe install --id "Canonical.Ubuntu" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
+
+# 虚拟机模拟器相关
+	# winget install --id Oracle.VirtualBox -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
+	# winget install --id Vmware.WorkstationPro -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
+	# winget install --id Hashicorp.Vagrant -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
+	# winget install --id Genymobile.Genymotion -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
+	# 最强的安卓模拟器
+	# winget install --id BlueStack.BlueStacks -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
+	# 只是安装位置, 主要还是虚拟机位置--location "D:\D:\DEV_ENV\BlueStacks"
--- boxstarter_file/winget_install.260417_普通用户.ps1
+++ boxstarter_file/winget_install.260417_普通用户.ps1
@@ -1,59 +1,26 @@
 # 会自动提权, 在电脑前点确定, 用管理员模式, ipip.besttrace无法忽略hash
 ## 开机启动
-winget.exe install --id "LiErHeXun.Quicker" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-	# winget.exe install --id "Yuanli.uTools" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809 # alt 空格
+
 ##时间管理
 winget.exe install --id "ClickUp.ClickUp" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809 # 看板
 
 
-##网盘
-	winget.exe install --id "gaozhangmin.XBYDriver" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-	## 小白羊运行一次后注释掉
-winget.exe install --id "Baidu.BaiduNetdisk" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-	## 夸克用沙盒运行
-
-##聊天
-winget.exe install --id "Tencent.TIM" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-
-##工具软件scoop不支持
-winget.exe install --id "Google.EarthPro" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-winget.exe install --id "Tonec.InternetDownloadManager" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
 winget.exe install --id "everauto.zTasker" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
 
-#其他
-winget.exe install --id "THS.THS" --location "D:\0LongTermData\0software" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-#沙盒
-winget.exe install --id "Sandboxie.Plus" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
 
-# 虚拟机模拟器相关
-	# winget install --id Oracle.VirtualBox -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
-	# winget install --id Vmware.WorkstationPro -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
-	# winget install --id Hashicorp.Vagrant -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
-	# winget install --id Genymobile.Genymotion -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
-	# 最强的安卓模拟器
-	# winget install --id BlueStack.BlueStacks -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809
-	# 只是安装位置, 主要还是虚拟机位置--location "D:\D:\DEV_ENV\BlueStacks"
 
 # 微信和阿里
 winget.exe install --id "Alibaba.AlipayKeyTool" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-winget.exe install --id "Tencent.WeixinDevTools" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
 
-# IDE
-winget.exe install --id "Microsoft.VisualStudio.Enterprise" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
-	## VYGRN-WPR22-HG4X3-692BF-QGT2V
 
 # WIN的app
 winget.exe install --id "9NBLGGH5R558" --exact --source msstore --accept-source-agreements --disable-interactivity --silent --accept-package-agreements  --proxy http://127.0.0.1:10809
   ## to do
-#winget.exe install --id "9PGCV4V3BK4W" --exact --source msstore --accept-source-agreements --disable-interactivity --silent --accept-package-agreements  --proxy http://127.0.0.1:10809 		
-  ##devtoys
+
 
 #开发小工具
 ##普通用户
 winget install --id IPIP.BestTrace -e --silent --accept-package-agreements --accept-source-agreements --proxy http://127.0.0.1:10809 --ignore-security-hash  
-
-#workbuddy
-winget.exe install --id "Tencent.WorkBuddy" --exact --source winget --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --proxy http://127.0.0.1:10809
 
 
 
